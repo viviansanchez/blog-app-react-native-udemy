2f114121f48ce329932945d0e3e9c6f3bb64dd1c
Vivian Sanchez Desktop <visasal88@gmail.com>
2025-01-14 18:25:22 -0300

    feat: create IndexScreen

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import BlogContext from "../context/BlogContext";
+
+const IndexScreen = () => {
+  const value = useContext(BlogContext);
+
+  return (
+    <View>
+      <Text>Index screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
