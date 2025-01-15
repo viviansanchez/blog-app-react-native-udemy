ddc14ba10d261a73ea81865c8c1ac9e86e6ceb36
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-15 15:43:46 -0300

    feat: display blog post that was clicked on in index screen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
 
-const ShowScreen = () => {
-  return (
-    <View>
-      <Text>ShowScreen</Text>
-    </View>
-  );
+const ShowScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+  const id = navigation.getParam("id");
+
+  const blogPost = state.find((blogPost) => blogPost.id === id);
+
+  return <View>{<Text>{blogPost.title}</Text>}</View>;
 };
 
 const styles = StyleSheet.create({});
