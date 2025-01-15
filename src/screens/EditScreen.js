7e01fe70d8a5a2afe6d0d061f70994ca40939154
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-15 17:42:29 -0300

    feat: create edit screen and add it to navigation

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const EditScreen = () => {
+  return (
+    <View>
+      <Text>Edit Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
