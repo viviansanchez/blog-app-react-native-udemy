cacf048140dc1600252bf48bd5ad90495bb51939
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-15 18:01:12 -0300

    feat: add form to edit title and content

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,10 +1,21 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Context } from "../context/BlogContext";
 
-const EditScreen = () => {
+const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam("id"));
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
     <View>
-      <Text>Edit Screen</Text>
+      <Text>Edit Title</Text>
+      <TextInput value={title} onChangeText={(newTitle) => setTitle(newTitle)} />
+      <Text>Edit Content</Text>
+      <TextInput value={content} onChangeText={(newContent) => setContent(newContent)} />
     </View>
   );
 };
