0b1664fe0d0c4fe969716f6d1dfae75502be6eb6
Vivian Sanchez Desktop <visasal88@gmail.com>
2025-01-16 11:05:01 -0300

    refactor: implement BlogPostForm

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,22 +1,21 @@
-import React, { useContext, useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
+  const { state, editBlogPost } = useContext(Context);
+  const id = navigation.getParam("id");
 
-  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam("id"));
-
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
+  const blogPost = state.find((blogPost) => blogPost.id === id);
 
   return (
-    <View>
-      <Text>Edit Title</Text>
-      <TextInput value={title} onChangeText={(newTitle) => setTitle(newTitle)} />
-      <Text>Edit Content</Text>
-      <TextInput value={content} onChangeText={(newContent) => setContent(newContent)} />
-    </View>
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop());
+      }}
+    />
   );
 };
 
